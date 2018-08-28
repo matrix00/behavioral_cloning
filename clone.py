72476ddc32f4d7cbee2dc88ecf4654059c7e00c4
matrix00 <bibah.nasha@gmail.com>
2018-08-28 05:33:31 +0000

    using LeNet to train the model

--- clone.py
+++ clone.py
@@ -29,11 +29,27 @@ y_train = np.array(measurements)
 
 #create a simple model for testing
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
+#model.add(Activation('softmax'))
+
+
+
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
 model.save('model.h5')
