8828552c6a3a3709f4c8315efae0b9a1c198e101
matrix00 <bibah.nasha@gmail.com>
2018-08-29 23:16:37 -0700

    Adding NVIDIA self driving model

--- clone.py
+++ clone.py
@@ -46,18 +46,37 @@ from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+"""
+    NVIDIA model
+    Normalization
+    Convolution: 5x5, filter: 24, strides: 2x2
+    Convolution: 5x5, filter: 36, strides: 2x2
+    Convolution: 5x5, filter: 48, strides: 2x2
+    Convolution: 3x3, filter: 64, strides: 1x1
+    Convolution: 3x3, filter: 64, strides: 1x1
+    Drop out (0.5)
+    Fully connected: neurons: 100
+    Fully connected: neurons: 50
+    Fully connected: neurons: 10
+    Fully connected: neurons: 1 (output)
+"""
+
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(Convolution2D(24,5,5, activation="relu"))
 model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5, activation="relu"))
+model.add(Convolution2D(36,5,5, activation="relu"))
 model.add(MaxPooling2D())
+model.add(Convolution2D(48,5,5, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(64,5,5, activation="relu"))
+model.add(Convolution2D(64,5,5, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+model.add(Dense(100))
+model.add(Dense(50))                                                                                                                 87%
+model.add(Dense(10))
 model.add(Dense(1))
-#model.add(Activation('softmax'))
 
 
 
