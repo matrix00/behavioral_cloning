48d2982492982aeb55dc91c57db4a5a7d5d05506
matrix00 <bibah.nasha@gmail.com>
2018-09-01 01:50:01 +0000

    add model to play back

--- clone.py
+++ clone.py
@@ -23,34 +23,38 @@ for line in lines:
 	measurement = float(line[3])
 	measurements.append(measurement)
 
-#augment images and with flipped images,
+#augment images and with flipped images, 
 #augment measurments with negative for flip images
 aug_images = []
 aug_measurements = []
 
 for image, measurement in zip(images, measurements):
-        aug_images.append(image)
-        aug_measurements.append(measurement)
-        aug_images.append(cv2.flip(image,1))
-        aug_measurements.append(measurement*-1.0)
+	aug_images.append(image)
+	aug_measurements.append(measurement)
+	aug_images.append(cv2.flip(image,1))
+	aug_measurements.append(measurement*-1.0)
 
 X_train = np.array(aug_images)
 y_train = np.array(aug_measurements)
 
 
+print(X_train.shape)
+
 #create a simple model for testing
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Cropping2D
+
 
 from keras.layers.core import Activation, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 """
-    NVIDIA model
+    NVIDIA model 
     Normalization
     Convolution: 5x5, filter: 24, strides: 2x2
-    Convolution: 5x5, filter: 36, strides: 2x2
+    Convolution: 5x5, filter: 36, strides: 2x2 
     Convolution: 5x5, filter: 48, strides: 2x2
     Convolution: 3x3, filter: 64, strides: 1x1
     Convolution: 3x3, filter: 64, strides: 1x1
@@ -61,8 +65,14 @@ from keras.layers.pooling import MaxPooling2D
     Fully connected: neurons: 1 (output)
 """
 
+
+#from keras import backend as K
+#K.set_image_dim_ordering('th')
+
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+#model.add(Cropping2D(cropping=((50,20), (0,0))))
+#model.add(Cropping2D(cropping=((75,25), (0,0))))
 model.add(Cropping2D(cropping=((30,10), (0,0))))
 model.add(Convolution2D(24,5,5, activation="relu"))
 model.add(MaxPooling2D())
@@ -75,9 +85,10 @@ model.add(Convolution2D(64,5,5, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
-model.add(Dense(50))                                                                                                                 87%
+model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
+
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
